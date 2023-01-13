66a621dacb04f8b21fcbbe7609bb3fe785924b1b
fernandobouchet <fernandobouchet@gmail.com>
2023-01-12 23:57:58 -0300

    Add a simple 'placeholder' while loading the images

--- frontend/src/components/ImageCard.tsx
+++ frontend/src/components/ImageCard.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { Button, Card } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import { Button, Card, Placeholder } from 'react-bootstrap';
 import { deleteImage } from '../services/imagesServices';
 
 interface Props {
@@ -12,6 +12,15 @@ interface Props {
 
 const ImageCard: React.FC<Props> = ({ image }) => {
   const [hover, setHover] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const newImage = new window.Image();
+    newImage.src = image.url;
+    newImage.onload = () => {
+      setIsLoaded(true);
+    };
+  }, [image]);
 
   const handleDelete = (id: string) => {
     deleteImage(id);
@@ -19,26 +28,42 @@ const ImageCard: React.FC<Props> = ({ image }) => {
 
   return (
     <Card
-      className="rounded-4"
+      className="rounded-4 border-0"
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
     >
-      <Card.Img
-        variant="top"
-        src={image.url}
-        id={image._id}
-        alt={image.label}
-        className={`rounded-4 imageCard ${hover ? 'hide' : ''}`}
-      />
-      <Card.Body className={`cardBody ${hover ? 'show' : ''}`}>
-        <Button
-          variant="outline-danger rounded-4 ms-auto"
-          onClick={() => handleDelete(image._id)}
+      {isLoaded ? (
+        <>
+          <Card.Img
+            onLoad={() => setIsLoaded(true)}
+            variant="top"
+            src={image.url}
+            id={image._id}
+            alt={image.label}
+            className={`rounded-4 imageCard ${hover ? 'hide' : ''}`}
+          />
+          <Card.Body className={`cardBody ${hover ? 'show' : ''}`}>
+            <Button
+              variant="outline-danger rounded-4 ms-auto"
+              onClick={() => handleDelete(image._id)}
+              size="sm"
+            >
+              delete
+            </Button>
+            <Card.Text className="text-white text-start">
+              {image.label}
+            </Card.Text>
+          </Card.Body>
+        </>
+      ) : (
+        <Placeholder
+          as="p"
+          animation="glow"
+          className={'rounded-4 placeholder'}
         >
-          delete
-        </Button>
-        <Card.Text className="text-white text-start">{image.label}</Card.Text>
-      </Card.Body>
+          <Placeholder className={'rounded-4 placeholder'} />
+        </Placeholder>
+      )}
     </Card>
   );
 };
