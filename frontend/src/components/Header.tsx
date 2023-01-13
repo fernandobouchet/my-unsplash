8ff0265edc2e0734b12f101594ad8343deff636f
fernandobouchet <fernandobouchet@gmail.com>
2023-01-13 01:01:23 -0300

    Updated 'Header'

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,6 @@
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form, InputGroup } from 'react-bootstrap';
 import logo from '../assets/my_unsplash_logo.svg';
+import searchLogo from '../assets/search_logo.svg';
 
 interface Props {
   setShow: React.Dispatch<React.SetStateAction<boolean>>;
@@ -7,13 +8,20 @@ interface Props {
 
 const Header: React.FC<Props> = ({ setShow }) => {
   return (
-    <header className="d-flex justify-content-between">
+    <header className="d-flex justify-content-between mt-4">
       <img src={logo} />
-      <Form className="d-flex w-100">
-        <Form.Group className="mb-3" controlId="formImageName">
-          <Form.Label></Form.Label>
-          <Form.Control type="email" placeholder="Search by name" />
-        </Form.Group>
+      <div className="d-flex w-100 ms-3">
+        <InputGroup id="input-group">
+          <InputGroup.Text id="search-icon-container">
+            <img src={searchLogo} />
+          </InputGroup.Text>
+          <Form.Control
+            id="search_input"
+            type="text"
+            placeholder="Search by name"
+            aria-label="image name"
+          />
+        </InputGroup>
         <Button
           variant="success"
           className="ms-auto my-auto"
@@ -21,7 +29,7 @@ const Header: React.FC<Props> = ({ setShow }) => {
         >
           Add a photo
         </Button>
-      </Form>
+      </div>
     </header>
   );
 };
